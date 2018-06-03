d5eabd421563297ad5512be71e7d62859c9ffb49
dafengshen <1548917336@qq.com>
2018-06-03 22:46:19 +0800

    Site updated: 2018-06-03 22:46:19

--- js/adsense.js
+++ js/adsense.js
@@ -1,7 +1,4 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-  (adsbygoogle = window.adsbygoogle || []).push({
+(adsbygoogle = window.adsbygoogle || []).push({
     google_ad_client: "ca-pub-7620585865130364",
     enable_page_level_ads: true
   });
-</script>
